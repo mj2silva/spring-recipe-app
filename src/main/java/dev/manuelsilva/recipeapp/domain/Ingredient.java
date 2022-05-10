39376066eba25b03457f037bc337ecdd13af0c32
Manuel Silva <manuel.silvag1@gmail.com>
2022-05-10 01:15:51 -0500

    Create recipe-ingredient entities and many-to-one and reverse relationship

---
+++ src/main/java/dev/manuelsilva/recipeapp/domain/Ingredient.java
@@ -0,0 +1,43 @@
+package dev.manuelsilva.recipeapp.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+    // TODO: Add unit of measure (Uom)
+    // private UnitOfMeasure uom;
+    @ManyToOne
+    private Recipe recipe;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
