9008d9a8be6159406076856fa1a55fae09935172
Manuel Silva <manuel.silvag1@gmail.com>
2022-05-10 08:27:21 -0500

    Added Difficulty enum and included in Recipe

---
+++ src/main/java/dev/manuelsilva/recipeapp/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package dev.manuelsilva.recipeapp.domain;
+
+public enum Difficulty {
+    EASY, MODERATE, HARD
+}
--- src/main/java/dev/manuelsilva/recipeapp/domain/Recipe.java
+++ src/main/java/dev/manuelsilva/recipeapp/domain/Recipe.java
@@ -16,8 +16,8 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    // TODO: Add difficulty
-    // private Difficulty difficulty;
+    @Enumerated(EnumType.STRING)
+    private Difficulty difficulty;
     @Lob
     private Byte[] image;
     @OneToOne(cascade = CascadeType.ALL)
@@ -87,6 +87,22 @@ public class Recipe {
 
     public void setDirections(String directions) {
         this.directions = directions;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 
     public Byte[] getImage() {
