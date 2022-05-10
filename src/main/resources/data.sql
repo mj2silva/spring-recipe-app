ff38c31936b4a5e4093346708cfb0b39f9baa05b
Manuel Silva <manuel.silvag1@gmail.com>
2022-05-10 10:20:18 -0500

    Initial data from data.sql
    - Initial categories
    - Initial units of measure

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+-- Initial categories
+INSERT INTO category (description) VALUES ('American');
+INSERT INTO category (description) VALUES ('Italian');
+INSERT INTO category (description) VALUES ('Mexican');
+INSERT INTO category (description) VALUES ('Peruvian');
+INSERT INTO category (description) VALUES ('Fast food');
+
+-- Initial units of measure
+INSERT INTO unit_of_measure (uom) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (uom) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure (uom) VALUES ('Cup');
+INSERT INTO unit_of_measure (uom) VALUES ('Pinch');
+INSERT INTO unit_of_measure (uom) VALUES ('Ounce');
