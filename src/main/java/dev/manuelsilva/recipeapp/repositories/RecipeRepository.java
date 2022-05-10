49c953e8403661f4e3aed9c5f2f0810652e5a6d7
Manuel Silva <manuel.silvag1@gmail.com>
2022-05-10 12:06:08 -0500

    Added recipe and unit of measure finders

--- src/main/java/dev/manuelsilva/recipeapp/repositories/RecipeRepository.java
+++ src/main/java/dev/manuelsilva/recipeapp/repositories/RecipeRepository.java
@@ -3,6 +3,8 @@ package dev.manuelsilva.recipeapp.repositories;
 import dev.manuelsilva.recipeapp.domain.Recipe;
 import org.springframework.data.repository.CrudRepository;
 
-public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+import java.util.Optional;
 
+public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+    Optional<Recipe> findByDescription(String description);
 }
--- src/main/java/dev/manuelsilva/recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/dev/manuelsilva/recipeapp/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,8 @@ package dev.manuelsilva.recipeapp.repositories;
 import dev.manuelsilva.recipeapp.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+    Optional<UnitOfMeasure> findByUom(String uom);
 }
