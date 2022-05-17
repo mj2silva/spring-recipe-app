ff81b8daed62eb4268492d648c7f907d153ebc16
Manuel Silva <manuel.silvag1@gmail.com>
2022-05-17 18:11:21 -0500

    Create new recipe form functionality - Added create recipe to the nav menu - Added th:field to the input fields in form template - Added corresponding ids for inputs - Fixed render of recipes with incomplete data

--- src/main/java/dev/manuelsilva/recipeapp/controllers/RecipeController.java
+++ src/main/java/dev/manuelsilva/recipeapp/controllers/RecipeController.java
@@ -5,9 +5,7 @@ import dev.manuelsilva.recipeapp.domain.Recipe;
 import dev.manuelsilva.recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/recipes")
@@ -36,5 +34,11 @@ public class RecipeController {
     public String newRecipe(Model model) {
         model.addAttribute("recipe", new RecipeCommand());
         return "recipes/form";
+    }
+
+    @PostMapping({"", "/"})
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+        return "redirect:/recipes/" + savedCommand.getId();
     }
 }
