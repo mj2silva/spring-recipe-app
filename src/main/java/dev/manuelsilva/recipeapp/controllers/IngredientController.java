d1c2bd01eae07d6db648dd82920354db706071b7
Manuel Silva <manuel.silvag1@gmail.com>
2022-05-17 20:39:54 -0500

    List ingredients if a recipe in separated view completed

---
+++ src/main/java/dev/manuelsilva/recipeapp/controllers/IngredientController.java
@@ -0,0 +1,28 @@
+package dev.manuelsilva.recipeapp.controllers;
+
+import dev.manuelsilva.recipeapp.commands.IngredientCommand;
+import dev.manuelsilva.recipeapp.commands.RecipeCommand;
+import dev.manuelsilva.recipeapp.services.RecipeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Set;
+
+@Controller
+public class IngredientController {
+    private final RecipeService recipeService;
+
+    public IngredientController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping("/recipes/{recipeId}/ingredients")
+    public String getIngredientsList(Model model, @PathVariable String recipeId) {
+        RecipeCommand command = recipeService.getRecipeCommandById(Long.valueOf(recipeId));
+        model.addAttribute("recipe", command);
+        return "recipes/ingredients/list";
+    }
+}
---
+++ src/test/java/dev/manuelsilva/recipeapp/controllers/IngredientControllerTest.java
@@ -0,0 +1,44 @@
+package dev.manuelsilva.recipeapp.controllers;
+
+import dev.manuelsilva.recipeapp.commands.RecipeCommand;
+import dev.manuelsilva.recipeapp.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+class IngredientControllerTest {
+    @Mock
+    RecipeService recipeService;
+    IngredientController controller;
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        controller = new IngredientController(recipeService);
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    void testListIngredients() throws Exception {
+        RecipeCommand recipeCommand = new RecipeCommand();
+        when(recipeService.getRecipeCommandById(anyLong())).thenReturn(recipeCommand);
+
+        mockMvc.perform(get("/recipes/1/ingredients"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipes/ingredients/list"))
+                .andExpect(model().attributeExists("recipe"));
+
+        verify(recipeService, times(1)).getRecipeCommandById(anyLong());
+    }
+}
